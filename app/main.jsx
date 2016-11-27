d55c1c632f4cf757381e03b2e5322032d53a1d26
webgua <shenxvdong1@gmail.com>
2016-11-27 20:02:36 +0800

    make the webpack work well

--- app/main.jsx
+++ app/main.jsx
@@ -1,6 +1,7 @@
-/**
- * Created by Sheldon on 2016/11/27.
- */
-var React = require('react');
-var AppComponent = require('./components/productBox.js');
-React.render(<AppComponent /> , document.getElementById('content'));
+// /**
+//  * Created by Sheldon on 2016/11/27.
+//  */
+// var React = require('react');
+// var AppComponent = require('./components/productBox.js');
+// React.render(<AppComponent /> , document.getElementById('content'));
+document.write('It works.')
