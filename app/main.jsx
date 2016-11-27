17bebc21c5617cf73756d1ce001d37915e37bf8b
webgua <shenxvdong1@gmail.com>
2016-11-28 02:38:35 +0800

    make the webpack+react work

--- app/main.jsx
+++ app/main.jsx
@@ -1,7 +1,9 @@
-// /**
-//  * Created by Sheldon on 2016/11/27.
-//  */
+/**
+ * Created by Sheldon on 2016/11/27.
+ */
 // var React = require('react');
-// var AppComponent = require('./components/productBox.js');
-// React.render(<AppComponent /> , document.getElementById('content'));
-document.write('It works.')
+var ReactDOM = require('react-dom');
+var React = require('react');
+var AppComponent = require('./components/productBox.js');
+ReactDOM.render(<AppComponent /> , document.getElementById('content'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,23 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './build'),
     filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+       }
+    }, {
+      test: /\.css$/,
+      loader: 'style!css'
+    }, {
+      test: /\.less$/,
+      loader: 'style!css!less'
+    },{ 
+      test: /\.(png|jpg)$/, 
+      loader: 'url?limit=25000' 
+    }]
   }
 };
